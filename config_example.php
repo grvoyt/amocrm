d4aed6c06f8d6f3e7ce3e74f2439cddb01442c67
George <grvoyt@ya.ru>
2018-06-26 12:14:22 +0300

    убрал в отдельный файл скрипт

---
+++ config_example.php
@@ -0,0 +1,5 @@
+<?php
+
+$email = 'test@test.tt'; //email на который зарегестрирован аккаунт
+$api_key = '63ad904a78c1b7eaaf389bb422836af1'; //api из кабинета пользователя
+$domain = 'publicdomain'; //поддомен в amocrm
--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?
 require_once 'amocrm.php';
+require_once 'config.php';
 
-$email = 'test@test.tt'; //email на который зарегестрирован аккаунт
-$api_key = '63ad904a78c1b7eaaf389bb422836af1'; //api из кабинета пользователя
-$domain = 'publicdomain'; //поддомен в amocrm
 
 $amo = new Amocrm($email,$api_key,$domain);
 
